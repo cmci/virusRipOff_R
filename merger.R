51780e161429f49d6a8adf9d897923cdbe9efcca
Kota Miura <miura@cmci.info>
2017-04-15 03:07:19 +0200

    added plotting parts.

--- merger.R
+++ merger.R
@@ -51,3 +51,31 @@ for (i in 1:length(datanames)){
     alldata = merge(alldata, dflist[[i]], all = TRUE)
   }
 }
+controlAll = merge(dflist[[1]], dflist[[2]], all = TRUE)
+
+alldata$RipOff_Density <- alldata$RipOff.counts5_10 / alldata$Area.um2. * 100
+alldata$RipOffPreDensity <- alldata$RipOff.counts1_4 / alldata$Area.um2. * 100
+
+        
+library("ggplot2")
+
+# checking the dependency of rip-off density to overall virus density, only control
+ggplot(subset(alldata, type %in% c("ctrl1", "ctrl2")), aes(x=Density, y = RipOff_Density)) + geom_point()
+# checking the dependency of rip-off density to overall virus density, all data
+ggplot(alldata, aes(x=Density, y = RipOff_Density)) + geom_point()
+
+
+ylabeltext = "Rip-Off-Density [counts/100um^2]"
+xordering = c("ctrl1", "ctrl2", "ROCKinh", "Blebb10um", "ClaD06", "CK666", "KOCLCAB", "SMIFH2", "Jaspla", "Genistein", "bcyclo", "a5b1", "beta1ABp5d2")
+ggplot(alldata, aes(x=type, y=RipOff_Density)) + geom_boxplot() + geom_jitter(width=0.2) + scale_x_discrete(name="Experiments", limits=xordering) + scale_y_continuous(name=ylabeltext)
+
+ylabeltext2 = "Rip-Off-Density (Before Adding Drug)\n[counts/100um^2]"
+ggplot(alldata, aes(x=type, y=RipOffPreDensity)) + geom_boxplot() + geom_jitter(width=0.2) + scale_x_discrete(name="Experiments", limits=xordering) + scale_y_continuous(name=ylabeltext2, limits = c(0, 7))
+
+#assessing changes of individuals (ratio of post/pre on per frame bases)
+alldata$RipOffChange <- alldata$RipOff_Density/alldata$RipOffPreDensity/6*4
+ylabeltext3 = "RipOff Change \npost / pre treatment per frame"
+ggplot(alldata, aes(x=type, y=RipOffChange)) + geom_boxplot() + geom_jitter(width=0.2) + scale_x_discrete(name="Experiments", limits=xordering) + scale_y_continuous(name=ylabeltext3)
+
+# only controls
+ggplot(subset(alldata, type %in% c("ctrl1", "ctrl2")), aes(x=type, y=RipOffChange)) + geom_boxplot() + geom_jitter(width=0.2) + scale_x_discrete(name="Experiments") + scale_y_continuous(name=ylabeltext3)
