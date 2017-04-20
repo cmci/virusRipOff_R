935e6d0b93b07bac0a6b254ba24ea004b5c815bf
Kota Miura <miura@cmci.info>
2017-04-20 23:03:30 +0200

    added time series part.

--- merger.R
+++ merger.R
@@ -1,5 +1,8 @@
 # Kota Miura April, 2017
 
+library("ggplot2")
+library("dplyr")
+
 gppath <- '/Users/miura/Dropbox/people/Tina/shared_Tina_Kota/Data Tina/'
 folders = c(
   "161122 ctrl croped and 16 frames",
@@ -18,22 +21,41 @@ folders = c(
   "170405 Hela croped and 16 frames"
 )
 
-datanames = c(
-  "ctrl1",
-  "ctrl2",
-  "ROCKinh",
-  "Blebb10um",
-  "ClaD06",
-  "CK666", 
-  "KOCLCAB",
-  "SMIFH2",
-  "Jaspla",
-  "Genistein",
-  "bcyclo",
-  "a5b1",
-  "beta1ABp5d2",
-  "Hela"
+datalist = list(
+  "ctrl1" = seq(5),
+  "ctrl2"= seq(8),
+  "ROCKinh" = c(0, 1, 2, 5, 6) + 1,
+  "Blebb10um" = c(0, 2, 3, 6, 7) + 1,
+  "ClaD06" = c(0, 1, 2) + 1,
+  "CK666"= c(0, 1, 2, 3, 7, 8) + 1, 
+  "KOCLCAB" = c(1, 2, 4, 5, 6, 7, 8, 9) + 1,
+  "SMIFH2" = c(1, 2, 3, 4, 5, 6, 7) + 1,
+  "Jaspla" = c(4) + 1,
+  "Genistein" = seq(11),
+  "bcyclo" = seq(9),
+  "a5b1" = c(0, 1, 9) + 1,
+  "beta1ABp5d2" = c(1, 2, 3, 4, 5, 6, 9) + 1,
+  "Hela" = c(0, 1, 2, 3, 4, 5, 6, 8, 9) + 1
 )
+
+datanames = names(datalist)
+  
+#   c(
+#   "ctrl1",
+#   "ctrl2",
+#   "ROCKinh",
+#   "Blebb10um",
+#   "ClaD06",
+#   "CK666", 
+#   "KOCLCAB",
+#   "SMIFH2",
+#   "Jaspla",
+#   "Genistein",
+#   "bcyclo",
+#   "a5b1",
+#   "beta1ABp5d2",
+#   "Hela"
+# )
 
 # prepare an empty list, which will hold all imported data as elements.
 # key = experiment type
@@ -70,7 +92,31 @@ alldata$RipOffChange <- alldata$RipOff_Density/alldata$RipOffPreDensity
 # Correction of Rip-Off density, as post-drug rip-offs are based on less virus. 
 alldata$RipOffChangeCorrected <- alldata$RipOffChange * ( alldata$Dots.Total / (alldata$Dots.Total - alldata$RipOff.counts1_4))
 
-library("ggplot2")
+### time series
+
+timecourseAll = data.frame()
+for (i in 1:length(folders)){
+  curfolder = file.path(gppath, folders[i])
+  exptype = names(datalist)[i]
+  items = length(datalist[[exptype]])
+  pathvector = character(length(items))
+  for (j in 1:items){
+    expnum =  datalist[[exptype]][j]
+    datapath = file.path(curfolder, paste('cell', expnum, '_virus_median.tif_counts.csv', sep=''))
+    curdata = read.csv(datapath)
+    curdata$Type = rep(names(datalist)[i], nrow(curdata))
+    curdata$CellID = rep(as.character(j), nrow(curdata))
+    cellarea = alldata$Area.um2.[alldata$CellID==expnum & alldata$type==exptype]
+    
+    curdata$RipOffDensity = curdata$Counts / cellarea * 100
+    curdata$VirusDensity = curdata$TotalCounts / cellarea * 100
+    timecourseAll = rbind(timecourseAll, curdata)
+    #pathvector[j] = datapath
+  }
+  #datafiles = lapply(pathvector, read.csv)
+}
+
+##### PLOTTINGS #####
 
 # checking the dependency of rip-off density to overall virus density, only control
 ggplot(subset(alldata, type %in% c("ctrl1", "ctrl2")), aes(x=Density, y = RipOff_Density)) + geom_point()
@@ -110,5 +156,53 @@ ggsave(paste('virusDensity_allExperiments.png'), width=25, height=15, units="cm"
 ylabeltext5 = "RipOff Change (Density Corrected) \npost / pre treatment per frame"
 ggplot(alldata, aes(x=type, y=RipOffChangeCorrected)) + geom_boxplot() + geom_jitter(width=0.2) + scale_x_discrete(name="Experiments", limits=xordering) + scale_y_continuous(name=ylabeltext5)
 ggsave(paste('RipOffDensityChanges_DensityCorrected.png'), width=25, height=20, units="cm")
+
+### plotting time series
+
+ggplot(data=timecourseAll) + 
+  geom_path(aes(x=Frame, y=Counts, color=CellID)) + 
+  scale_x_continuous(name = 'Time [Frame]', limits=c(1, 16)) + 
+  scale_y_continuous(name='Rip-Off [Counts]') + 
+  facet_wrap(~ Type, nrow = 4)
+ggsave('TimeCourse_Counts.png', width=25, height=20, units="cm")
+
+for (thename in names(datalist)){
+  typemax = max(timecourseAll$Counts[timecourseAll$Type==thename])
+  annoY = typemax/2
+  ggplot(data=filter(timecourseAll, Type==thename)) + 
+    geom_path(aes(x=Frame, y=Counts, color=CellID)) + 
+    scale_x_continuous(name = 'Time [Frame]', limits=c(1, 16)) + 
+    scale_y_continuous(name='Rip-Off [Counts]') +
+    annotate("text", x=12, y=annoY, label= thename, size=30, color='gray')
+  ggsave(paste("TimeCourse_Counts_",thename,".png", sep=''), width=25, height=20, units="cm")
+}
+
+ggplot(data=timecourseAll) + geom_path(aes(x=Frame, y=RipOffDensity, color=CellID)) + scale_x_continuous(name = 'Time [Frame]', limits=c(1, 16)) + scale_y_continuous(name='Rip-Off Density \n[counts/100um^2]') + facet_wrap(~ Type, nrow = 4)
+ggsave(paste('TimeCourse_RipOffDensity.png'), width=25, height=20, units="cm")
+
+for (thename in names(datalist)){
+  typemax = max(timecourseAll$RipOffDensity[timecourseAll$Type==thename])
+  annoY = typemax/2
+  ggplot(data=filter(timecourseAll, Type==thename)) + 
+    geom_path(aes(x=Frame, y=RipOffDensity, color=CellID)) + 
+    scale_x_continuous(name = 'Time [Frame]', limits=c(1, 16)) + 
+    scale_y_continuous(name='Rip-Off Density \n[counts/100um^2]') +
+    annotate("text", x=12, y=annoY, label= thename, size=30, color='gray')
+  ggsave(paste("TimeCourse_RipOffDensity_",thename,".png", sep=''), width=25, height=20, units="cm")
+}
+
+ggplot(data=timecourseAll) + geom_path(aes(x=Frame, y=VirusDensity, color=CellID)) + scale_x_continuous(name = 'Time [Frame]', limits=c(1, 16)) + scale_y_continuous(name='Virus Density \n[counts/100um^2]') + facet_wrap(~ Type, nrow = 4)
+ggsave(paste('TimeCourse_VirusDensity.png'), width=25, height=20, units="cm")
+
+for (thename in names(datalist)){
+  typemax = max(timecourseAll$VirusDensity[timecourseAll$Type==thename])
+  annoY = typemax/2
+  ggplot(data=filter(timecourseAll, Type==thename)) + 
+    geom_path(aes(x=Frame, y=VirusDensity, color=CellID)) + 
+    scale_x_continuous(name = 'Time [Frame]', limits=c(1, 16)) + 
+    scale_y_continuous(name='Virus Density \n[counts/100um^2]', limits=c(0, NA)) +
+    annotate("text", x=12, y=annoY, label= thename, size=30, color='gray')
+  ggsave(paste("TimeCourse_VirusDensity_",thename,".png", sep=''), width=25, height=20, units="cm")
+}
 
                                                           
