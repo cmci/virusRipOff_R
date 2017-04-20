43558d895458fa12de2bef1e7684d8010ee38d60
Kota Miura <miura@cmci.info>
2017-04-20 02:04:14 +0200

    as now uses 3 frames both for pre and post, no need of correction by difference in the sampling frame numbers.

--- merger.R
+++ merger.R
@@ -75,7 +75,8 @@ ylabeltext2 = "Rip-Off-Density (Before Adding Drug)\n[counts/100um^2]"
 ggplot(alldata, aes(x=type, y=RipOffPreDensity)) + geom_boxplot() + geom_jitter(width=0.2) + scale_x_discrete(name="Experiments", limits=xordering) + scale_y_continuous(name=ylabeltext2)
 
 #assessing changes of individuals (ratio of post/pre on per frame bases)
-alldata$RipOffChange <- alldata$RipOff_Density/alldata$RipOffPreDensity/6*4
+#alldata$RipOffChange <- alldata$RipOff_Density/alldata$RipOffPreDensity/6*4
+alldata$RipOffChange <- alldata$RipOff_Density/alldata$RipOffPreDensity
 
 ylabeltext3 = "RipOff Change \npost / pre treatment per frame"
 ggplot(alldata, aes(x=type, y=RipOffChange)) + geom_boxplot() + geom_jitter(width=0.2) + scale_x_discrete(name="Experiments", limits=xordering) + scale_y_continuous(name=ylabeltext3)
