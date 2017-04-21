00a2151976200630c29c703d18f990c20f2d7e42
Kota Miura <miura@cmci.info>
2017-04-22 01:28:07 +0200

    removed hard coded infos, added UI for input file choosing, put all plots in a folder.

--- merger.R
+++ merger.R
@@ -3,52 +3,23 @@
 library("ggplot2")
 library("dplyr")
 
-# gppath <- '/Users/miura/Dropbox/people/Tina/shared_Tina_Kota/Data Tina/'
-# folders = c(
-#   "161122 ctrl croped and 16 frames",
-#   "161206 ctrl croped and 16 frames",  
-#   "161129 ROCKinh croped and 16 frames",
-#   "161205 10umblebb croped and 16 frames",
-#   "161222 claD0.06 croped and 16 frames",
-#   "170130 CK666 croped and 16 frames",
-#   "170206 KOCLCAB croped and 50 frames",
-#   "170215 SMIFH2 croped and 16 frames",
-#   "170217 Jasplakinolide croped and 16 frames",
-#   "170221 Jasplakinolide croped and 16 frames",
-#   "170222 Genistein croped and 16 frames",
-#   "170302 bcyclo croped and 16 frames",
-#   "170307 a5b1peptidomimetic croped and 16 frames",
-#   "170328 beta1ABp5d2 croped and 14 frames",
-#   "170405 Hela croped and 16 frames"
-# )
-# 
-# datalist = list(
-#   "ctrl1" = seq(5),
-#   "ctrl2"= seq(8),
-#   "ROCKinh" = c(0, 1, 2, 5, 6) + 1,
-#   "Blebb10um" = c(0, 2, 3, 6, 7) + 1,
-#   "ClaD06" = c(0, 1, 2) + 1,
-#   "CK666"= c(0, 1, 2, 3, 7, 8) + 1, 
-#   "KOCLCAB" = c(1, 2, 4, 5, 6, 7, 8, 9) + 1,
-#   "SMIFH2" = c(1, 2, 3, 4, 5, 6, 7) + 1,
-#   "Jaspla" = c(4) + 1,
-#   "Jaspla2" = c(1, 2, 3, 4) + 1,
-#   "Genistein" = seq(11),
-#   "bcyclo" = seq(9),
-#   "a5b1" = c(0, 1, 2, 4, 5, 6, 7, 8, 9, 10) + 1,
-#   "beta1ABp5d2" = c(1, 2, 3, 4, 5, 6, 9) + 1,
-#   "Hela" = c(0, 1, 2, 3, 4, 5, 6, 8, 9) + 1
-# )
-
-#inputcsv = read.csv('/Users/miura/Desktop/20170421_test/test2.csv')
 inputcsv = read.csv('/Users/miura/Dropbox/people/Tina/shared_Tina_Kota/data_lists/test.csv')
+#inputcsv = file.choose()
+
+### load "results.csv" 
 gppath = as.character(inputcsv$root[1])
 folders = as.character(inputcsv$datafolder)
 datanames = as.character(inputcsv$type)
+
+# prepare an empty list, which will hold all imported data as elements.
+# key = experiment type
+# value = dataframe, imported from ImageJ results
 dflist <- vector("list", length(datanames))
+# fill names first
 names(dflist) <- datanames
 datalist <- vector("list", length(datanames))
 names(datalist) <- datanames
+# adding values (data) to each keys
 for (ind in 1:length(datanames)){
   print(datanames[ind])
   print(file.path(gppath, folders[[ind]], "results.csv"))
@@ -59,39 +30,18 @@ for (ind in 1:length(datanames)){
   datalist[[datanames[ind]]] <- ids
 }
 
-datanames = names(datalist)
+#datanames = names(datalist)
 
 ## box plot order
 xordering = c("ctrl1", "ctrl2", "ROCKinh", "Blebb10um", "ClaD06", "CK666", "KOCLCAB", "SMIFH2", "Jaspla", "Jaspla2", "Genistein", "bcyclo", "a5b1", "beta1ABp5d2", "Hela")
   
-#   c(
-#   "ctrl1",
-#   "ctrl2",
-#   "ROCKinh",
-#   "Blebb10um",
-#   "ClaD06",
-#   "CK666", 
-#   "KOCLCAB",
-#   "SMIFH2",
-#   "Jaspla",
-#   "Genistein",
-#   "bcyclo",
-#   "a5b1",
-#   "beta1ABp5d2",
-#   "Hela"
-# )
-
-# prepare an empty list, which will hold all imported data as elements.
-# key = experiment type
-# value = dataframe, imported from ImageJ results
-dflist <- vector("list", length(datanames))
-# fill names first
-names(dflist) <- datanames
-# adding values (data) to each keys
-for (ind in 1:length(datanames)){
-  dflist[[datanames[ind]]] <- read.csv2( file.path(gppath, folders[[ind]], "results.csv"), sep=",", dec=".")
-  dflist[[datanames[ind]]]$type = rep(datanames[[ind]], nrow(dflist[[datanames[ind]]]))
-}
+# dflist <- vector("list", length(datanames))
+# names(dflist) <- datanames
+# 
+# for (ind in 1:length(datanames)){
+#   dflist[[datanames[ind]]] <- read.csv2( file.path(gppath, folders[[ind]], "results.csv"), sep=",", dec=".")
+#   dflist[[datanames[ind]]]$type = rep(datanames[[ind]], nrow(dflist[[datanames[ind]]]))
+# }
 
 # merge all dataframes in the above dictionary in a single dataframe
 for (i in 1:length(datanames)){
@@ -104,20 +54,6 @@ for (i in 1:length(datanames)){
 
 # specifically create a data frame for all controls. 
 controlAll = merge(dflist[[1]], dflist[[2]], all = TRUE)
-
-# compute pre / post drug treatment RipOff density. 
-alldata$RipOffPreDensity <- alldata$RipOff.counts2_4 / alldata$Area.um2. * 100
-alldata$RipOff_Density <- alldata$RipOff.counts5_7 / alldata$Area.um2. * 100
-alldata$RipOff2_Density <- alldata$RipOff.counts8_10 / alldata$Area.um2. * 100
-
-#assessing changes of individuals (ratio of post/pre on per frame bases)
-#alldata$RipOffChange <- alldata$RipOff_Density/alldata$RipOffPreDensity/6*4
-alldata$RipOffChange <- alldata$RipOff_Density/alldata$RipOffPreDensity
-alldata$RipOff2Change <- alldata$RipOff2_Density/alldata$RipOffPreDensity
-
-# Correction of Rip-Off density, as post-drug rip-offs are based on less virus. 
-alldata$RipOffChangeCorrected <- alldata$RipOffChange * ( alldata$Dots.Total / (alldata$Dots.Total - alldata$RipOff.counts2_4))
-alldata$RipOff2ChangeCorrected <- alldata$RipOffChange * ( alldata$Dots.Total / (alldata$Dots.Total - alldata$RipOff.counts2_4 - alldata$RipOff.counts5_7))
 
 ### time series
 
@@ -145,64 +81,91 @@ for (i in 1:length(folders)){
   #datafiles = lapply(pathvector, read.csv)
 }
 
+### compute paramters
+# compute pre / post drug treatment RipOff density. 
+alldata$RipOffPreDensity <- alldata$RipOff.counts2_4 / alldata$Area.um2. * 100
+alldata$RipOff_Density <- alldata$RipOff.counts5_7 / alldata$Area.um2. * 100
+alldata$RipOff2_Density <- alldata$RipOff.counts8_10 / alldata$Area.um2. * 100
+
+#assessing changes of individuals (ratio of post/pre on per frame bases)
+#alldata$RipOffChange <- alldata$RipOff_Density/alldata$RipOffPreDensity/6*4
+alldata$RipOffChange <- alldata$RipOff_Density/alldata$RipOffPreDensity
+alldata$RipOff2Change <- alldata$RipOff2_Density/alldata$RipOffPreDensity
+
+# Correction of Rip-Off density, as post-drug rip-offs are based on less virus. 
+alldata$RipOffChangeCorrected <- alldata$RipOffChange * ( alldata$Dots.Total / (alldata$Dots.Total - alldata$RipOff.counts2_4))
+alldata$RipOff2ChangeCorrected <- alldata$RipOffChange * ( alldata$Dots.Total / (alldata$Dots.Total - alldata$RipOff.counts2_4 - alldata$RipOff.counts5_7))
+
 ##### PLOTTINGS #####
+
+outfoldername = strftime(Sys.time(), "%Y%m%d-%H%M")
+outfolderpath = file.path(gppath, "plots", outfoldername)
+if ( !dir.exists( outfolderpath )) {
+  dir.create( outfolderpath )
+}
+
+outsubfoldername = file.path(outfolderpath, 'time_courses')
+if ( !dir.exists(outsubfoldername)) {
+  dir.create( outsubfoldername )
+}
 
 # checking the dependency of rip-off density to overall virus density, only control
 ggplot(subset(alldata, type %in% c("ctrl1", "ctrl2")), aes(x=Density, y = RipOff_Density)) + geom_point()
-ggsave(paste('Control_RipOffDensityVSdensity.png'), width=25, height=20, units="cm")
+ggsave(file.path(outfolderpath , 'Control_RipOffDensityVSdensity.png'), width=25, height=20, units="cm")
 
 # checking the dependency of rip-off density to overall virus density, all data
 ggplot(alldata, aes(x=Density, y = RipOff_Density)) + geom_point()
-ggsave(paste('All_RipOffDensityVSdensity.png'), width=25, height=20, units="cm")
+ggsave(file.path(outfolderpath , 'All_RipOffDensityVSdensity.png'), width=25, height=20, units="cm")
 
 # checking the dependency of rip-off (pre-drug) density to overall virus density, all data
 ggplot(alldata, aes(x=Density, y = RipOffPreDensity)) + geom_point()
-ggsave(paste('All_RipOffPreDensityVSdensity.png'), width=25, height=20, units="cm")
+ggsave(file.path(outfolderpath , 'All_RipOffPreDensityVSdensity.png'), width=25, height=20, units="cm")
 
 
 ylabeltext = "Rip-Off-Density\nF5-F7 [counts/100um^2]"
 ggplot(alldata, aes(x=type, y=RipOff_Density)) + geom_boxplot() + geom_jitter(width=0.2) + scale_x_discrete(name="Experiments", limits=xordering) + scale_y_continuous(name=ylabeltext)
-ggsave(paste('RipOff_Density_allExperiments.png'), width=25, height=15, units="cm")
+ggsave(file.path(outfolderpath , 'RipOff_Density_allExperiments.png'), width=25, height=15, units="cm")
 
 ylabeltext = "Rip-Off-Density\nF8-F10 [counts/100um^2]"
 ggplot(alldata, aes(x=type, y=RipOff_Density)) + geom_boxplot() + geom_jitter(width=0.2) + scale_x_discrete(name="Experiments", limits=xordering) + scale_y_continuous(name=ylabeltext)
-ggsave(paste('RipOff_Density2_allExperiments.png'), width=25, height=15, units="cm")
+ggsave(file.path(outfolderpath , 'RipOff_Density2_allExperiments.png'), width=25, height=15, units="cm")
 
 ylabeltext2 = "Rip-Off-Density (Before Adding Drug)\n[counts/100um^2]"
 ggplot(alldata, aes(x=type, y=RipOffPreDensity)) + geom_boxplot() + geom_jitter(width=0.2) + scale_x_discrete(name="Experiments", limits=xordering) + scale_y_continuous(name=ylabeltext2)
-ggsave(paste('RipOff_Density_allExperiments_BeforeDrug.png'), width=25, height=15, units="cm")
+ggsave(file.path(outfolderpath , 'RipOff_Density_allExperiments_BeforeDrug.png'), width=25, height=15, units="cm")
 
 ylabeltext3 = "RipOff Change \npost[5-7] / pre treatment per frame"
 ggplot(alldata, aes(x=type, y=RipOffChange)) + geom_boxplot() + geom_jitter(width=0.2) + scale_x_discrete(name="Experiments", limits=xordering) + scale_y_continuous(name=ylabeltext3)
-ggsave(paste('RipOffDensityChanges.png'), width=25, height=20, units="cm")
+ggsave(file.path(outfolderpath , 'RipOffDensityChanges.png'), width=25, height=20, units="cm")
 
 ylabeltext3 = "RipOff Change \npost[8-10] / pre treatment per frame"
 ggplot(alldata, aes(x=type, y=RipOff2Change)) + geom_boxplot() + geom_jitter(width=0.2) + scale_x_discrete(name="Experiments", limits=xordering) + scale_y_continuous(name=ylabeltext3)
-ggsave(paste('RipOff2DensityChanges.png'), width=25, height=20, units="cm")
+ggsave(file.path(outfolderpath , 'RipOff2DensityChanges.png'), width=25, height=20, units="cm")
 
 # only controls
 ggplot(subset(alldata, type %in% c("ctrl1", "ctrl2")), aes(x=type, y=RipOffChange)) + geom_boxplot() + geom_jitter(width=0.2) + scale_x_discrete(name="Experiments") + scale_y_continuous(name=ylabeltext3)
-ggsave(paste('RipOffDensityChanges_OnlyControls.png'), width=25, height=20, units="cm")
+ggsave(file.path(outfolderpath , 'RipOffDensityChanges_OnlyControls.png'), width=25, height=20, units="cm")
 
 ggplot(subset(alldata, type %in% c("ctrl1", "ctrl2")), aes(x=type, y=RipOff2Change)) + geom_boxplot() + geom_jitter(width=0.2) + scale_x_discrete(name="Experiments") + scale_y_continuous(name=ylabeltext3)
-ggsave(paste('RipOff2DensityChanges_OnlyControls.png'), width=25, height=20, units="cm")
+ggsave(file.path(outfolderpath , 'RipOff2DensityChanges_OnlyControls.png'), width=25, height=20, units="cm")
 
 # RipOff changes, with corrections
 ylabeltext5 = "RipOff Change (Density Corrected) \npost[5-7] / pre treatment per frame"
 ggplot(alldata, aes(x=type, y=RipOffChangeCorrected)) + geom_boxplot() + geom_jitter(width=0.2) + scale_x_discrete(name="Experiments", limits=xordering) + scale_y_continuous(name=ylabeltext5)
-ggsave(paste('RipOffDensityChanges_DensityCorrected.png'), width=25, height=20, units="cm")
+ggsave(file.path(outfolderpath , 'RipOffDensityChanges_DensityCorrected.png'), width=25, height=20, units="cm")
 
 ylabeltext5 = "RipOff Change (Density Corrected) \npost[8-10] / pre treatment per frame"
 ggplot(alldata, aes(x=type, y=RipOff2ChangeCorrected)) + geom_boxplot() + geom_jitter(width=0.2) + scale_x_discrete(name="Experiments", limits=xordering) + scale_y_continuous(name=ylabeltext5)
-ggsave(paste('RipOff2DensityChanges_DensityCorrected.png'), width=25, height=20, units="cm")
+ggsave(file.path(outfolderpath , 'RipOff2DensityChanges_DensityCorrected.png'), width=25, height=20, units="cm")
 
 # checking the density for each type (VirusDensity_in_each_Experiment)
 ylabeltext4 = "Virus Density in 2nd frame\n [counts/um^2]"
 ggplot(alldata, aes(x=type, y = Density)) + geom_boxplot() + geom_jitter(width=0.2) + scale_x_discrete(name="Experiments", limits=xordering) + scale_y_continuous(name=ylabeltext4)
-ggsave(paste('virusDensity_allExperiments.png'), width=25, height=15, units="cm")
+ggsave(file.path(outfolderpath , 'virusDensity_allExperiments.png'), width=25, height=15, units="cm")
 
 
 ### plotting time series
+
 
 timecourseAllF2 = filter(timecourseAll, Frame >= 2, Frame <= 16)
 
@@ -211,7 +174,7 @@ ggplot(data = timecourseAllF2)  +
   scale_x_continuous(name = 'Time [Frame]', limits=c(2, 16)) + 
   scale_y_continuous(name='Rip-Off [Counts]', limits = NULL) + 
   facet_wrap(~ Type, nrow = 4)
-ggsave('TimeCourse_Counts.png', width=25, height=20, units="cm")
+ggsave(file.path(outsubfoldername , 'TimeCourse_Counts.png'), width=25, height=20, units="cm")
 
 for (thename in names(datalist)){
   typemax = max(timecourseAllF2$Counts[timecourseAllF2$Type==thename])
@@ -221,7 +184,7 @@ for (thename in names(datalist)){
     scale_x_continuous(name = 'Time [Frame]', limits=c(2, 16)) + 
     scale_y_continuous(name='Rip-Off [Counts]', limits = NULL) +
     annotate("text", x=12, y=annoY, label= thename, size=30, color='gray')
-  ggsave(paste("TimeCourse_Counts_",thename,".png", sep=''), width=25, height=20, units="cm")
+  ggsave(file.path(outsubfoldername , paste("TimeCourse_Counts_",thename,".png", sep='')), width=25, height=20, units="cm")
 }
 
 ggplot(data= timecourseAllF2) + 
@@ -229,7 +192,7 @@ ggplot(data= timecourseAllF2) +
   scale_x_continuous(name = 'Time [Frame]', limits=c(2, 16)) + 
   scale_y_continuous(name='Rip-Off Density \n[counts/100um^2]', limits = NULL) + 
   facet_wrap(~ Type, nrow = 4)
-ggsave(paste('TimeCourse_RipOffDensity.png'), width=25, height=20, units="cm")
+ggsave(file.path(outsubfoldername , paste('TimeCourse_RipOffDensity.png')), width=25, height=20, units="cm")
 
 for (thename in names(datalist)){
   typemax = max(timecourseAllF2$RipOffDensity[timecourseAllF2$Type==thename])
@@ -239,7 +202,7 @@ for (thename in names(datalist)){
     scale_x_continuous(name = 'Time [Frame]', limits=c(2, 16)) + 
     scale_y_continuous(name='Rip-Off Density \n[counts/100um^2]', limits = NULL) +
     annotate("text", x=12, y=annoY, label= thename, size=30, color='gray')
-  ggsave(paste("TimeCourse_RipOffDensity_",thename,".png", sep=''), width=25, height=20, units="cm")
+  ggsave(file.path(outsubfoldername , paste("TimeCourse_RipOffDensity_",thename,".png", sep='')), width=25, height=20, units="cm")
 }
 
 ggplot(data= timecourseAllF2) + 
@@ -247,7 +210,7 @@ ggplot(data= timecourseAllF2) +
   scale_x_continuous(name = 'Time [Frame]', limits=c(2, 16)) + 
   scale_y_continuous(name='Virus Density \n[counts/100um^2]', limits = NULL) + 
   facet_wrap(~ Type, nrow = 4)
-ggsave(paste('TimeCourse_VirusDensity.png'), width=25, height=20, units="cm")
+ggsave(file.path(outsubfoldername , paste('TimeCourse_VirusDensity.png')), width=25, height=20, units="cm")
 
 for (thename in names(datalist)){
   typemax = max(timecourseAllF2$VirusDensity[timecourseAllF2$Type==thename])
@@ -258,7 +221,7 @@ for (thename in names(datalist)){
     scale_x_continuous(name = 'Time [Frame]', limits=c(2, 16)) + 
     scale_y_continuous(name='Virus Density \n[counts/100um^2]', limits = NULL ) +
     annotate("text", x=12, y=annoY, label= thename, size=30, color='gray')
-  ggsave(paste("TimeCourse_VirusDensity_",thename,".png", sep=''), width=25, height=20, units="cm")
+  ggsave(file.path(outsubfoldername , paste("TimeCourse_VirusDensity_",thename,".png", sep='')), width=25, height=20, units="cm")
 }
 
                                                           
